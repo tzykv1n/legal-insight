8ab7091d200175146c99fda56b53085b80be7e38
Legal-Insight <166642106+Legal-Insight@users.noreply.github.com>
2024-04-11 20:56:51 +0530

    Delete installnec.sh

--- installnec.sh
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-result = subprocess.run(['winget', 'install', '--id' , '9NRWMJP3717K'], capture_output=True, text=True)
-if result.returncode == 0:
-    print(result.stdout)
-else:
-    print("Error:", result.stderr) 
